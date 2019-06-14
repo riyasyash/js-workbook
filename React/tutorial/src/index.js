e35b60b6a56a05afbd84ac392241f46fdb7b58d1
Riyas P <riyasyash@gmail.com>
2019-06-14 22:55:40 +0530

    taking turns

--- React/tutorial/src/index.js
+++ React/tutorial/src/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
   renderSquare(i) {
@@ -29,11 +28,14 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X'
-    this.setState({ squares: squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !(this.state.xIsNext),
+    });
   }
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
