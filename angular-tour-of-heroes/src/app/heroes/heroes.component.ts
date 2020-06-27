756e37cc74c5094c1a561fe327e581414e88845f
Riyas P <riyasyash@gmail.com>
2020-06-27 20:37:03 +0530

    added angular tour of heroes

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
